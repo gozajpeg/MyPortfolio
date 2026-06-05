cab7bc7a4766ad9d97d269e8433262f11a823e96
gozajpeg <goza.543113@wnu.sti.edu.ph>
2026-06-06 00:37:15 +0800

    1st npm project

---
+++ src/data/Projects/Others/npmData.js
@@ -0,0 +1,15 @@
+import { logoAssets } from '../../../assets/logo/logo';
+
+const { gozalogoImg } = logoAssets;
+export const npmData = {
+  categoryTitle: 'Open Source / NPM Packages',
+  link: 'Unreleased',
+  items: [
+    {
+      title: '@ragoza/regex-easy',
+      description: 'A high-performance, tree-shakeable regular expression builder. Allows developers to fluently chain regex patterns, easily extract named capture groups, and inject predefined validated patterns with zero dependencies.',
+      tech: 'JavaScript / Node.js',
+      image: gozalogoImg,
+    }
+  ]
+};
